230afc4862106d457bb6c941ec6f1b7148f39002
paridis <rhizoma_paridis@outlook.com>
2026-09-17 10:33:05 +0800

    feat(gitsigns): 光标行行尾虚拟文本显示 blame 信息

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,7 +1,20 @@
 return {
     "lewis6991/gitsigns.nvim",
     event = "VeryLazy",
-    opts = {
+    config = function()
+        vim.api.nvim_set_hl(0, "GitSignsCurrentLineBlame", { link = "Comment" })
+        require("gitsigns").setup({
+            current_line_blame = true,
+            current_line_blame_opts = {
+            virt_text = true,
+            virt_text_pos = "eol",
+            delay = 500,
+            ignore_whitespace = false,
+            virt_text_priority = 100,
+            format = function(name, date, msg)
+                return string.format("%s • %s • %s", name, date, msg)
+            end,
+        },
         on_attach = function(buffer)
             local wk = require("which-key")
             local gs = require("gitsigns")
@@ -15,6 +28,6 @@ return {
                 }
             })
         end
-    },
-
+        })
+    end,
 }
